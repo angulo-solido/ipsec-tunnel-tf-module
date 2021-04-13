92dea9525fc4a8c10af7bac658baae8ff609d829
Tiago Carrondo <tcarrondo@angulosolido.pt>
2021-04-13 14:05:59 +0100

    upgrade terraform

--- versions.tf
+++ versions.tf
@@ -1,6 +1,5 @@
 terraform {
-  # We want to hold off on 0.13 until we have tested it.
-  required_version = "~> 0.12.0"
+  required_version = "~> 0.13.0"
 
   # If you use any other providers you should also pin them to the
   # major version currently being used.  This practice will help us
